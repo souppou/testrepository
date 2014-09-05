41c8aa55bc8901e9937c85975b4cab4ab54b00d8
souppou <souppou83@gmail.com>
2014-09-05 17:41:50 +0530

    message

--- test.php
+++ test.php
@@ -5,12 +5,11 @@ echo "done1";
 echo "done2";
 echo "done3";
 echo "done4";
-<<<<<<< HEAD
 echo "done5";
 echo "done6";
-=======
->>>>>>> parent of cb10aed... message
 echo "done7";
+echo "done8";
+echo "done9";
 
 exit;
 
